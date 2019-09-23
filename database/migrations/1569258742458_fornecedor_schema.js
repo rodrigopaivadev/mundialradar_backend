0c3080fadff7c3ca9e059b3cdba9963e16d9c36c
lucaspaiva1 <lukspaiva95@gmail.com>
2019-09-23 15:05:57 -0300

    migration de fornecedor

--- database/migrations/1569258742458_fornecedor_schema.js
+++ database/migrations/1569258742458_fornecedor_schema.js
@@ -5,14 +5,25 @@ const Schema = use('Schema')
 
 class FornecedorSchema extends Schema {
   up () {
-    this.create('fornecedors', (table) => {
+    this.create('fornecedores', (table) => {
       table.increments()
+      table.string('nome').notNullable()
+      table.string('email').nullable()
+      table.string('telefone').notNullable()
+      table.string('documento').notNullable()
+      table.string('tipo_documento').notNullable()
+      table.string('endereco').nullable()
+      table.string('numero').nullable()
+      table.string('bairro').nullable()
+      table.string('complemento').nullable()
+      table.string('cidade').nullable()
+      table.string('cep').nullable()
       table.timestamps()
     })
   }
 
   down () {
-    this.drop('fornecedors')
+    this.drop('fornecedores')
   }
 }
 
