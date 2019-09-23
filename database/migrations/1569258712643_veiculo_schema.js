9fde66f3683ff3dc666165ed12ba0f8f78a5450b
lucaspaiva1 <lukspaiva95@gmail.com>
2019-09-23 15:20:27 -0300

    migration de veiculos

--- database/migrations/1569258712643_veiculo_schema.js
+++ database/migrations/1569258712643_veiculo_schema.js
@@ -4,14 +4,42 @@
 const Schema = use('Schema')
 
 class VeiculoSchema extends Schema {
-  up () {
+  up() {
     this.create('veiculos', (table) => {
       table.increments()
+      table.string('fabricante').nullable()
+      table.string('modelo').nullable()
+      table.string('placa').notNullable()
+      table.string('cor').nullable()
+      table.string('chassi').nullable()
+      table.string('renavam').nullable()
+      table.string('combustivel').nullable()
+      table.string('identificacao').nullable()
+      table.boolean('possui_bloqueio').notNullable()
+      table.text('descricao_bloqueio').nullable()
+      table.text('local_equipamento').nullable()
+      table.text('observacao').nullable()
+      table
+        .integer('id_grupo')
+        .unsigned()
+        .nullable()
+        .references('id')
+        .inTable('grupos')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE')
+      table
+        .integer('id_rastreador')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('rastreadores')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE')
       table.timestamps()
     })
   }
 
-  down () {
+  down() {
     this.drop('veiculos')
   }
 }
