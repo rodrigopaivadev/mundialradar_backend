f561a4ce99d9bb46bb9a4ae9d6eb21472c4e1278
lucaspaiva1 <lukspaiva95@gmail.com>
2019-09-23 15:43:01 -0300

    migration de chips

--- database/migrations/1569258686364_chip_schema.js
+++ database/migrations/1569258686364_chip_schema.js
@@ -1,19 +1,22 @@
-'use strict'
+'use strict';
 
 /** @type {import('@adonisjs/lucid/src/Schema')} */
-const Schema = use('Schema')
+const Schema = use('Schema');
 
 class ChipSchema extends Schema {
-  up () {
-    this.create('chips', (table) => {
-      table.increments()
-      table.timestamps()
-    })
+  up() {
+    this.create('chips', table => {
+      table.increments();
+      table.string('operadora').notNullable();
+      table.string('linha').notNullable();
+      table.string('numero').notNullable();
+      table.timestamps();
+    });
   }
 
-  down () {
-    this.drop('chips')
+  down() {
+    this.drop('chips');
   }
 }
 
-module.exports = ChipSchema
+module.exports = ChipSchema;
