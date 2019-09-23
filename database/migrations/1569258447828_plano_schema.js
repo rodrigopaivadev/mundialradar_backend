be10be456555f37ed503d81a71eedd7cbd53b50d
lucaspaiva1 <lukspaiva95@gmail.com>
2019-09-23 16:04:32 -0300

    migration de plano

--- database/migrations/1569258447828_plano_schema.js
+++ database/migrations/1569258447828_plano_schema.js
@@ -1,19 +1,23 @@
-'use strict'
+'use strict';
 
 /** @type {import('@adonisjs/lucid/src/Schema')} */
-const Schema = use('Schema')
+const Schema = use('Schema');
 
 class PlanoSchema extends Schema {
-  up () {
-    this.create('planos', (table) => {
-      table.increments()
-      table.timestamps()
-    })
+  up() {
+    this.create('planos', table => {
+      table.increments();
+      table.string('descricao').notNullable();
+      table.string('valor').notNullable();
+      table.string('tipo').notNullable();
+      table.string('status').notNullable();
+      table.timestamps();
+    });
   }
 
-  down () {
-    this.drop('planos')
+  down() {
+    this.drop('planos');
   }
 }
 
-module.exports = PlanoSchema
+module.exports = PlanoSchema;
