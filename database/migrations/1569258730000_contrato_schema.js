96d44930fbe3b811741756ea51253939be22c8c7
lucaspaiva1 <lukspaiva95@gmail.com>
2019-09-23 15:14:11 -0300

    migration de contrato

--- database/migrations/1569258730000_contrato_schema.js
+++ database/migrations/1569258730000_contrato_schema.js
@@ -4,15 +4,39 @@
 const Schema = use('Schema')
 
 class ContratoSchema extends Schema {
-  up () {
-    this.create('contratoes', (table) => {
+  up() {
+    this.create('contratos', (table) => {
       table.increments()
+      table
+        .integer('id_cliente')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('clientes')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE')
+      table
+        .integer('id_veiculo')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('veiculos')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE')
+      table
+        .integer('id_plano')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('planos')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE')
       table.timestamps()
     })
   }
 
-  down () {
-    this.drop('contratoes')
+  down() {
+    this.drop('contratos')
   }
 }
 
